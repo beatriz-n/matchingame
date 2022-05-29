32b959fa07d99bd18412fd63b116f7c09167c1f3
beatriz <armizando132@gmail.com>
2022-05-29 09:41:01 -0300

    master

---
+++ src/components/cardGame/index.js
@@ -0,0 +1,11 @@
+import "./style.css";
+
+function cardGame(){
+    return`
+        <article class="card-game">
+        <img src="img/alura-pixel.png">
+        </article>
+    `;
+     
+    }
+    export default cardGame;
