320170ec547111a26061fe6f7c4dd967194388e0
beatriz <armizando132@gmail.com>
2022-05-29 09:41:40 -0300

    master

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+import "./src/style/settings/colors.css";
+import "./src/style/elements/base.css";
+
 import cardGame from "./src/components/cardGame";
 
 const $root = document.querySelector('#root');
